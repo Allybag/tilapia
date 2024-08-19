66a5b7df6028ead056993bdac755a1a22afb4416
Ally <bagkingally@gmail.com>
2024-08-19 16:58:12 -0400

    tcp: Attempt to read a packet from TAP Device

--- src/tcp.cpp
+++ src/tcp.cpp
@@ -6,4 +6,18 @@ int main()
 {
     TapDevice tap{};
     std::println("Created tap device {} : descriptor {}", tap.name(), tap.descriptor());
+
+    int messagesRemaining{1};
+
+    char buffer[2000];
+    while (messagesRemaining)
+    {
+        if (read(tap.descriptor(), buffer, sizeof(buffer)) < 0)
+        {
+            std::print("Failed to read from Tap Device");
+        }
+
+        messagesRemaining -= 1;
+        std::print("Received a message of size {}", strlen(buffer));
+    }
 }
