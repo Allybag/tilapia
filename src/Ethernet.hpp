304b99f417b760bf7d9d909a745118b2b387ca46
Ally <bagkingally@gmail.com>
2024-08-20 16:08:26 -0400

    Move generic header parsing logic to src/Headers.hpp

--- src/Ethernet.hpp
+++ src/Ethernet.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <algorithm>
-#include <bit>
+#include <Headers.hpp>
+
 #include <cstdint>
 #include <cstring>
 #include <format>
@@ -29,62 +29,11 @@ struct EthernetHeader
 };
 static_assert(sizeof(EthernetHeader) == 14, "Ethernet header must be 14 bytes long");
 
-template <std::size_t... Sizes>
-constexpr std::size_t totalSize(std::index_sequence<Sizes...> sizes)
-{
-    if constexpr (sizes.size() == 0)
-    {
-        return 0;
-    }
-    else
-    {
-        return (Sizes + ...);
-    }
-}
-
-template <typename HeaderT>
-struct LayoutInfo
-{
-    static constexpr std::index_sequence<> Sizes{};
-
-    static_assert(totalSize(Sizes) == sizeof(HeaderT));
-};
-
 template <>
 struct LayoutInfo<EthernetHeader>
 {
     static constexpr std::index_sequence<6, 6, 2> Sizes{};
 };
-
-template <std::size_t ArraySize, std::size_t MemberSize>
-void byteswapMember(std::array<std::byte, ArraySize>& bytes, std::size_t& offset)
-{
-    std::ranges::reverse(bytes.begin() + offset, bytes.begin() + offset + MemberSize);
-    offset += MemberSize;
-}
-
-template <std::size_t ArraySize, std::size_t... MemberSizes>
-void byteswapMembers(std::array<std::byte, ArraySize>& bytes, std::index_sequence<MemberSizes...>)
-{
-    std::size_t offset{0};
-    (byteswapMember<ArraySize, MemberSizes>(bytes, offset), ...);
-}
-
-template <typename HeaderT>
-auto fromWire(const char* buffer) -> HeaderT
-{
-    if constexpr (LayoutInfo<HeaderT>::Sizes.size() == 0)
-    {
-        throw std::runtime_error{"No layout info for requested type"};
-    }
-
-    std::array<std::byte, sizeof(HeaderT)> bytes;
-    std::memcpy(&bytes, buffer, sizeof(bytes));
-
-    byteswapMembers(bytes, LayoutInfo<HeaderT>::Sizes);
-
-    return std::bit_cast<HeaderT>(bytes);
-}
 
 struct SimpleFormatter
 {
---
+++ src/Headers.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <algorithm>
+#include <cstddef>
+#include <stdexcept>
+#include <utility>
+
+template <std::size_t... Sizes>
+constexpr std::size_t totalSize(std::index_sequence<Sizes...> sizes)
+{
+    if constexpr (sizes.size() == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return (Sizes + ...);
+    }
+}
+
+template <typename HeaderT>
+struct LayoutInfo
+{
+    static constexpr std::index_sequence<> Sizes{};
+
+    static_assert(totalSize(Sizes) == sizeof(HeaderT));
+};
+
+
+template <std::size_t ArraySize, std::size_t MemberSize>
+void byteswapMember(std::array<std::byte, ArraySize>& bytes, std::size_t& offset)
+{
+    std::ranges::reverse(bytes.begin() + offset, bytes.begin() + offset + MemberSize);
+    offset += MemberSize;
+}
+
+template <std::size_t ArraySize, std::size_t... MemberSizes>
+void byteswapMembers(std::array<std::byte, ArraySize>& bytes, std::index_sequence<MemberSizes...>)
+{
+    std::size_t offset{0};
+    (byteswapMember<ArraySize, MemberSizes>(bytes, offset), ...);
+}
+
+template <typename HeaderT>
+auto fromWire(const char* buffer) -> HeaderT
+{
+    if constexpr (LayoutInfo<HeaderT>::Sizes.size() == 0)
+    {
+        throw std::runtime_error{"No layout info for requested type"};
+    }
+
+    std::array<std::byte, sizeof(HeaderT)> bytes;
+    std::memcpy(&bytes, buffer, sizeof(bytes));
+
+    byteswapMembers(bytes, LayoutInfo<HeaderT>::Sizes);
+
+    return std::bit_cast<HeaderT>(bytes);
+}
+
